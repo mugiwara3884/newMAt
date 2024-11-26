c91473ad7a8f53c241c7819b06aa9f02f40dfde3
mugiwara3884 <amishra12345678@gmail.com>
2024-11-25 17:09:53 -0700

    chnages

--- api/server.js
+++ api/server.js
@@ -1,39 +1,35 @@
-const { parse } = require('querystring');
+const express = require('express');
 
-module.exports = (req, res) => {
-    if (req.method === 'POST') {
-        let body = '';
+const app = express();
 
-        // Collect data from the POST request
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
+// Middleware for parsing form data
+app.use(express.urlencoded({ extended: true }));
 
-        req.on('end', () => {
-            const { noun, verb, adjective, place, pluralNoun } = parse(body);
+// Routes
+app.post('/generate-madlib', (req, res) => {
+    const { noun, verb, adjective, place, pluralNoun } = req.body;
 
-            // Validate input
-            if (!noun || !verb || !adjective || !place || !pluralNoun) {
-                return res.status(400).send(`
-                    <h1>Error: Incomplete Form</h1>
-                    <p>Please fill in all the fields!</p>
-                    <a href="/ITC505/lab-7/index.html">Go Back</a>
-                `);
-            }
-
-            // Generate the Mad Lib
-            const madLib = `
-                In a ${adjective} ${place}, there was a ${noun} who loved to ${verb}.
-                They always played with ${pluralNoun} and had a great time!
-            `;
-
-            return res.status(200).send(`
-                <h1>Your Mad Lib</h1>
-                <p>${madLib}</p>
-                <a href="/ITC505/lab-7/index.html">Create Another</a>
-                `);
-        });
-    } else {
-        res.status(405).send('Method Not Allowed');
+    // Validate input
+    if (!noun || !verb || !adjective || !place || !pluralNoun) {
+        return res.status(400).send(`
+            <h1>Error: Incomplete Form</h1>
+            <p>Please fill in all the fields!</p>
+            <a href="/ITC505/lab-7/index.html">Go Back</a>
+        `);
     }
-};
+
+    // Generate the Mad Lib
+    const madLib = `
+        In a ${adjective} ${place}, there was a ${noun} who loved to ${verb}.
+        They always played with ${pluralNoun} and had a great time!
+    `;
+
+    res.status(200).send(`
+        <h1>Your Mad Lib</h1>
+        <p>${madLib}</p>
+        <a href="/ITC505/lab-7/index.html">Create Another</a>
+    `);
+});
+
+// Export the app as a serverless function
+module.exports = app;
